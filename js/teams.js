c68a9dd32ae06bc7aaf4bb1cd60f3c8523a2c5ff
Rubén Chavarría <txingo@gmail.com>
2022-10-19 11:21:09 +0200

    Extract teams into a separate array

---
+++ js/teams.js
@@ -0,0 +1,98 @@
+const teams = {
+  descansa: {
+    name: '** DESCANSA **',
+    logo: 'des-logo',
+    stadium: {
+      address: 'n/a',
+      map: 'https://maps.google.com',
+    },
+  },
+  alboradaInf: {
+    name: 'BASKET ALBORADA RSDA',
+    logo: 'alb-logo',
+    stadium: {
+      address: 'Colegio Alborada, Alejo Carpentier, 27',
+      map: 'https://maps.google.com',
+    },
+  },
+  paracuellos: {
+    name: 'BALONCESTO PARACUELLOS',
+    logo: 'par-logo',
+    stadium: {
+      address: 'Sala Escolar Virgen de la Rivera, camino Radar, 1',
+      map: 'https://maps.google.com',
+    },
+  },
+  boadilla: {
+    name: 'BOADILLA MONSTERS',
+    logo: 'mon-logo',
+    stadium: {
+      address: 'Pabellon Felipe VI, Miguel Angel Cantero Oliva, 4',
+      map: 'https://maps.google.com',
+    },
+  },
+  osuna: {
+    name: 'ALAMEDA DE OSUNA B',
+    logo: 'osu-logo',
+    stadium: {
+      address: 'IES Alameda de Osuna, Antonio Sancha, 11, entrada por Brezos 2',
+      map: 'https://maps.google.com',
+    },
+  },
+  alcobendas: {
+    name: 'VALCUDE ALCOBENDAS B',
+    logo: 'val-logo',
+    stadium: {
+      address: 'Pabellón Amaya Valdemoro, crta Alcobendas-Barajas, km 1,200',
+      map: 'https://maps.google.com',
+    },
+  },
+  isaac: {
+    name: 'ISAAC NEWTON',
+    logo: 'new-logo',
+    stadium: {
+      address: 'IES Isaac Newton, Isla Malaita, 9',
+      map: 'https://maps.google.com',
+    },
+  },
+  paz: {
+    name: 'LA PAZ C.D. VERDE',
+    logo: 'paz-logo',
+    stadium: {
+      address: 'CP Miguel Delibes, Felipe II, 15',
+      map: 'https://maps.google.com',
+    },
+  },
+  fenix: {
+    name: 'FENIX BASKET CLUB',
+    logo: 'fen-logo',
+    stadium: {
+      address: 'Deportivo Municipal Ángel Nieto, crta Pozuelo-Boadilla Km 4',
+      map: 'https://maps.google.com',
+    },
+  },
+  salesianos: {
+    name: 'SALESIANOS SOTO PIRATAS',
+    logo: 'sal-logo',
+    stadium: {
+      address: 'Pabellón Soto del Real, El Egidillo, s/n',
+      map: 'https://maps.google.com',
+    },
+  },
+  revolution: {
+    name: 'REVOLUTION',
+    logo: 'rev-logo',
+    stadium: {
+      address: 'Av de los Poblados, 42',
+      map: 'https://maps.google.com',
+    },
+  },
+  veritas: {
+    name: 'VERITAS POZUELO NEGRO',
+    logo: 'ver-logo',
+    stadium: {
+      address: 'Instituto Veritas, Av Radio Televisión, 2',
+      map: 'https://maps.google.com',
+    },
+  },
+}
