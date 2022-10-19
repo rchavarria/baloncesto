55d5cda866e02f4689ae23ee70c13185addf4919
Rubén Chavarría <txingo@gmail.com>
2022-10-19 18:03:45 +0200

    Add labels for three types of matches

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 function App() {
+  const nextScheduledMatchIndex = 1;
+
   const played = matches.filter(m => m.played);
   const notPlayed = matches.filter(m => !m.played);
 
   return <>
+    <h1>Próximo partido</h1>
+    <ScheduledMatch match={matches[nextScheduledMatchIndex]} />
+
+    <h1>Partidos jugados</h1>
     { played.map((match, index) => <PlayedMatch key={index} match={match} />) }
+
+    <h1>Partidos por jugar</h1>
     { notPlayed.map((match, index) => <ScheduledMatch key={index} match={match} />) }
   </>;
 }
