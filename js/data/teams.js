8c8dbcc2b6cf0f2be45c49187656ae178c85d151
Rubén Chavarría <txingo@gmail.com>
2022-10-20 16:21:55 +0200

    Add team's links to stadium location

--- js/data/teams.js
+++ js/data/teams.js
@@ -12,7 +12,7 @@ const teams = {
     logo: 'alb-logo',
     stadium: {
       address: 'Colegio Alborada, Alejo Carpentier, 27',
-      map: 'https://maps.google.com',
+      map: 'https://g.page/ColegioAlborada',
     },
     web: 'https://fbm.es/equipo-807696-7022/basket-alborada-rsda',
   },
@@ -21,7 +21,7 @@ const teams = {
     logo: 'par-logo',
     stadium: {
       address: 'Sala Escolar Virgen de la Rivera, camino Radar, 1',
-      map: 'https://maps.google.com',
+      map: 'https://goo.gl/maps/dWtvodFmdJmFjirY9',
     },
     web: 'https://fbm.es/equipo-806354-7022/baloncesto-paracuellos',
   },
@@ -30,7 +30,7 @@ const teams = {
     logo: 'mon-logo',
     stadium: {
       address: 'Pabellon Felipe VI, Miguel Angel Cantero Oliva, 4',
-      map: 'https://maps.google.com',
+      map: 'https://goo.gl/maps/6qyeGE4hgFcUGVQJA',
     },
     web: 'https://fbm.es/equipo-806731-7022/boadilla-monsters',
   },
@@ -39,7 +39,7 @@ const teams = {
     logo: 'osu-logo',
     stadium: {
       address: 'IES Alameda de Osuna, Antonio Sancha, 11, entrada por Brezos 2',
-      map: 'https://maps.google.com',
+      map: 'https://goo.gl/maps/Ht9kjnXKyDLQPBnW8',
     },
     web: 'https://fbm.es/equipo-808579-7022/alameda-de-osuna-b',
   },
@@ -48,7 +48,7 @@ const teams = {
     logo: 'val-logo',
     stadium: {
       address: 'Pabellón Amaya Valdemoro, crta Alcobendas-Barajas, km 1,200',
-      map: 'https://maps.google.com',
+      map: 'https://goo.gl/maps/QYrq9cDTn8rAvjMb7',
     },
     web: 'https://fbm.es/equipo-807910-7022/valcude-alcobendas-b',
   },
@@ -57,7 +57,7 @@ const teams = {
     logo: 'new-logo',
     stadium: {
       address: 'IES Isaac Newton, Isla Malaita, 9',
-      map: 'https://maps.google.com',
+      map: 'https://goo.gl/maps/uR4WhYqrgTxsBXNs6',
     },
     web: 'https://fbm.es/equipo-808272-7022/isaac-newton',
   },
@@ -66,7 +66,7 @@ const teams = {
     logo: 'paz-logo',
     stadium: {
       address: 'CP Miguel Delibes, Felipe II, 15',
-      map: 'https://maps.google.com',
+      map: 'https://goo.gl/maps/WSisfE2BLgYrtMSC6',
     },
     web: 'https://fbm.es/equipo-808571-7022/la-paz-cd-verde',
   },
@@ -75,7 +75,7 @@ const teams = {
     logo: 'fen-logo',
     stadium: {
       address: 'Deportivo Municipal Ángel Nieto, crta Pozuelo-Boadilla Km 4',
-      map: 'https://maps.google.com',
+      map: 'https://goo.gl/maps/vnhE3HPEKKE7SVFt7',
     },
     web: 'https://fbm.es/equipo-808406-7022/fenix-basket-club',
   },
@@ -84,7 +84,7 @@ const teams = {
     logo: 'sal-logo',
     stadium: {
       address: 'Pabellón Soto del Real, El Egidillo, s/n',
-      map: 'https://maps.google.com',
+      map: 'https://goo.gl/maps/DWsrqvpp4UXYsrXN8',
     },
     web: 'https://fbm.es/equipo-808308-7022/salesianos-soto-piratas-',
   },
@@ -92,8 +92,8 @@ const teams = {
     name: 'REVOLUTION',
     logo: 'rev-logo',
     stadium: {
-      address: 'Av de los Poblados, 42',
-      map: 'https://maps.google.com',
+      address: 'Av de los Poblados, 72',
+      map: 'https://g.page/CDMLasCruces',
     },
     web: 'https://fbm.es/equipo-807993-7022/revolution-',
   },
@@ -102,7 +102,7 @@ const teams = {
     logo: 'ver-logo',
     stadium: {
       address: 'Instituto Veritas, Av Radio Televisión, 2',
-      map: 'https://maps.google.com',
+      map: 'https://goo.gl/maps/WjbKLd3JSmByM2yF6',
     },
     web: 'https://fbm.es/equipo-808509-7022/veritas-pozuelo-negro',
   },
