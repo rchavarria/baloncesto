5ec966d3843de7501e49882c713a1460106a01af
Rubén Chavarría <txingo@gmail.com>
2022-10-23 18:15:07 +0200

    Add teams for "alevín" category

--- js/data/teams.js
+++ js/data/teams.js
@@ -1,4 +1,13 @@
 const teams = {
+  template: {
+    name: 'name',
+    logo: 'img/logs/none.jpg',
+    stadium: {
+      address: 'address',
+      map: 'https://maps.google.com',
+    },
+    web: 'https://web.com'
+  },
   descansa: {
     name: '** DESCANSA **',
     logo: 'des-logo',
@@ -6,10 +15,11 @@ const teams = {
       address: 'n/a',
       map: 'https://maps.google.com',
     },
+    web: 'https://web.com'
   },
-  alboradaInf: {
+  alboradaSenior: {
     name: 'BASKET ALBORADA RSDA',
-    logo: 'img/logos/alborada-infantil.jpg',
+    logo: 'img/logos/alborada.jpg',
     stadium: {
       address: 'Colegio Alborada, Alejo Carpentier, 27',
       map: 'https://g.page/ColegioAlborada',
@@ -70,7 +80,7 @@ const teams = {
     },
     web: 'https://fbm.es/equipo-808571-7022/la-paz-cd-verde',
   },
-  fenix: {
+  fenixSenior: {
     name: 'FENIX BASKET CLUB',
     logo: 'img/logos/fenix.jpg',
     stadium: {
@@ -105,5 +115,95 @@ const teams = {
       map: 'https://goo.gl/maps/WjbKLd3JSmByM2yF6',
     },
     web: 'https://fbm.es/equipo-808509-7022/veritas-pozuelo-negro',
+  },
+  alboradaJunior: {
+    name: 'BASKET ALBORADA RSDA',
+    logo: 'img/logos/alborada.jpg',
+    stadium: {
+      address: 'Colegio Alborada, Alejo Carpentier, 27',
+      map: 'https://g.page/ColegioAlborada',
+    },
+    web: 'https://fbm.es/equipo-807697-7085/basket-alborada-rsda',
+  },
+  valle: {
+    name: 'EL VALLE C.D. A',
+    logo: 'img/logos/none.jpg',
+    stadium: {
+      address: 'EL VALLE SANCHINARRO, COLEGIO ANA DE AUSTRIA, 60 (Entrada Vicente Blasco Ibañez), Madrid',
+      map: 'https://maps.google.es',
+    },
+    web: 'https://fbm.es/equipo-806284-7085/el-valle-cd-a',
+  },
+  pilaristas: {
+    name: 'PILARISTAS',
+    logo: 'img/logos/pilaristas.jpg',
+    stadium: {
+      address: 'PILARISTAS, COLEGIO, CASTELLO, 56',
+      map: 'https://g.page/ElPilarMadrid',
+    },
+    web: 'https://fbm.es/equipo-807202-7085/pilaristas',
+  },
+  spinola: {
+    name: 'SPÍNOLA CHAMARTÍN',
+    logo: 'img/logos/spinola.jpg',
+    stadium: {
+      address: 'SPINOLA CHAMARTIN, COLEGIO, CARDENAL MARCELO SPINOLA, 34',
+      map: 'https://goo.gl/maps/4DRmdMgs8Pd2Axyo7',
+    },
+    web: 'https://fbm.es/equipo-808435-7085/spinola-chamartin',
+  },
+  aranjuez: {
+    name: 'OLÍMPICO ARANJUEZ',
+    logo: 'img/logos/aranjuez.jpg',
+    stadium: {
+      address: 'LAS OLIVAS, CIUDAD DEPORTIVA, ISIDRO GONZALEZ VELAZQUEZ, S/N',
+      map: 'https://goo.gl/maps/6NvGmfct3S24kQmL9',
+    },
+    web: 'https://fbm.es/equipo-807103-7085/olimpico-aranjuez',
+  },
+  saltium: {
+    name: 'SALTIUM ALCORCON BASKET B',
+    logo: 'img/logos/saltium.jpg',
+    stadium: {
+      address: 'LOS CANTOS, PABELLON PISTA 4, LOS CANTOS, 28',
+      map: 'https://goo.gl/maps/hRtzhKYSx5dvuTaYA',
+    },
+    web: 'https://fbm.es/equipo-806314-7085/saltium-alcorc%c3%b3n-basket-b',
+  },
+  fenixJunior: {
+    name: 'FENIX BASKET CLUB',
+    logo: 'img/logos/fenixJunior.jpg',
+    stadium: {
+      address: 'Deportivo Municipal Ángel Nieto, crta Pozuelo-Boadilla Km 4',
+      map: 'https://goo.gl/maps/vnhE3HPEKKE7SVFt7',
+    },
+    web: 'https://fbm.es/equipo-808751-7085/fenix-basket-club',
+  },
+  sanfernando: {
+    name: 'CLUB BALONCESTO SAN FERNANDO',
+    logo: 'img/logos/sanfernando.jpg',
+    stadium: {
+      address: 'PARQUE HENARES, PABELLON, calle VITORIA, S/N',
+      map: 'https://goo.gl/maps/tfCdjBdjmWpdoZTr8',
+    },
+    web: 'https://fbm.es/equipo-808438-7085/hyundai-almoauto-san-fernando-2011',
+  },
+  maravillas: {
+    name: 'LA SALLE MARAVILLAS 11',
+    logo: 'img/logos/maravillas.jpg',
+    stadium: {
+      address: 'LA SALLE MARAVILLAS, COLEGIO, GUADALQUIVIR, 9',
+      map: 'https://goo.gl/maps/YFw5XALS4qkhVFdA6',
+    },
+    web: 'https://fbm.es/equipo-808538-7085/maravillas-11',
+  },
+  fuenlabrada: {
+    name: 'BALONCESTO FUENLABRADA',
+    logo: 'img/logos/fuenlabrada.jpg',
+    stadium: {
+      address: 'EL ARROYO, PABELLON, FUENTESAUCO, 1',
+      map: 'https://goo.gl/maps/4jHQkBhDBQ4G9H3c8',
+    },
+    web: 'https://fbm.es/equipo-805705-7085/baloncesto-fuenlabrada',
   },
 }
