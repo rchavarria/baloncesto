fc5dea7f9930ae341061281fc1939459298c9da1
Rubén Chavarría <txingo@gmail.com>
2023-04-17 15:48:02 +0200

    Einar stats for 2023 (first)

---
+++ js/data/einar-stats-2023.js
@@ -0,0 +1,626 @@
+const einarStats2023 = [
+  {
+    round: 'num jornada',
+    date: '2022-10-22T00:00:00',
+    team: {
+      short: 'nombre corto',
+      name: 'nombre largo',
+      venue: 'donde juega',
+    },
+    time: {
+      minutes: 26,
+      seconds: 47,
+    },
+    points: 19,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 18,
+      three: 1,
+    },
+    faults: 'num faltas recibidas',
+    valuation: 'valoracion (aunque no se como se calcula)',
+  },
+  {
+    round: 1,
+    date: '2022-10-22T00:00:00',
+    team: {
+      short: 'evc',
+      name: 'evc',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 26,
+      seconds: 47,
+    },
+    points: 19,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 18,
+      three: 1,
+    },
+    faults: 2,
+    valuation: 30,
+  },
+  {
+    round: 2,
+    date: '2022-11-05T00:00:00',
+    team: {
+      short: 'bfu',
+      name: 'Baloncesto Fuenlabrada',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 31,
+      seconds: 7,
+    },
+    points: 18,
+    throws: {
+      free: {
+        successful: 2,
+        total: 10,
+      },
+      two: 8,
+      three: 0,
+    },
+    faults: 5,
+    valuation: 0,
+  },
+  {
+    round: 3,
+    date: '2022-11-19T00:00:00',
+    team: {
+      short: 'has',
+      name: 'has',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 29,
+    throws: {
+      free: {
+        successful: 1,
+        total: 3,
+      },
+      two: 14,
+      three: 0,
+    },
+    faults: 3,
+    valuation: 36,
+  },
+  {
+    round: 4,
+    date: '2022-11-25T00:00:00',
+    team: {
+      short: 'pil',
+      name: 'Pilaristas',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 31,
+      seconds: 45,
+    },
+    points: 41,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 19,
+      three: 1,
+    },
+    faults: 0,
+    valuation: 62,
+  },
+  {
+    round: 5,
+    date: '2022-12-10T00:00:00',
+    team: {
+      short: 'sab',
+      name: 'sab',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 35,
+    throws: {
+      free: {
+        successful: 7,
+        total: 8,
+      },
+      two: 14,
+      three: 0,
+    },
+    faults: 4,
+    valuation: 23,
+  },
+  {
+    round: 6,
+    date: '2022-12-17T00:00:00',
+    team: {
+      short: 'fbc',
+      name: 'Fénix Basket Club',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 31,
+    throws: {
+      free: {
+        successful: 2,
+        total: 4,
+      },
+      two: 13,
+      three: 1,
+    },
+    faults: 0,
+    valuation: 20,
+  },
+  {
+    round: 7,
+    date: '2022-12-18T00:00:00',
+    team: {
+      short: 'oar',
+      name: 'oar',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 28,
+      seconds: 54,
+    },
+    points: 23,
+    throws: {
+      free: {
+        successful: 3,
+        total: 5,
+      },
+      two: 10,
+      three: 0,
+    },
+    faults: 4,
+    valuation: 33,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+  {
+    round: 0,
+    date: '2023-00-00T00:00:00',
+    team: {
+      short: 'x',
+      name: 'x',
+      venue: 'g.maps',
+    },
+    time: {
+      minutes: 32,
+      seconds: 0,
+    },
+    points: 33,
+    throws: {
+      free: {
+        successful: 0,
+        total: 0,
+      },
+      two: 0,
+      three: 0,
+    },
+    faults: 0,
+    valuation: 0,
+  },
+]
