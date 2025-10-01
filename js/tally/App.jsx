8a59f44ecd06ced6d6b732be95b256ad262ebb81
Rubén Chavarría <txingo@gmail.com>
2025-10-01 07:47:00 +0200

    ✨ Tallies can be customized

---
+++ js/tally/App.jsx
@@ -0,0 +1,55 @@
+const App = () => {
+  const [tallies, setTallies] = React.useState('99-00');
+
+  const handleInputChange = (event) => {
+    setTallies(event.target.value);
+  };
+
+  return (
+    <div>
+      <div className="data-input">
+        <input
+          type="text"
+          value={tallies}
+          onChange={handleInputChange}
+          placeholder="Escribe algo..."
+        />
+        <p>Valor actual: {tallies}</p>
+      </div>
+
+      <div className="flex-container">
+        <div className="row header-row">
+          <div className="col col-fbm">
+            <img src="img/new-logos/fbm-logo-colores-2025-no-ibrcj.png"/>
+          </div>
+          <div className="col col-headers">
+            <div className="header-title category">Cadete 2011</div>
+            <div className="header-title result">🤩 Resultado Final</div>
+          </div>
+        </div>
+
+        <div className="row row-home-team">
+          <div className="col">
+            CB ALCOBENDAS
+          </div>
+        </div>
+
+        <div className="row row-visit-team">
+          <div className="col">
+            ZENTRO BASKET
+          </div>
+        </div>
+
+        <div className="row row-results">
+          <div className="col">
+            <img className="logo" src="img/new-logos/alcobendas-transparent.png"/>
+          </div>
+          <Tallies tallies={tallies} />
+          <div className="col">
+            <img className="logo" src="img/new-logos/zentro-transparent.png"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
