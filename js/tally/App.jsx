2432e7ee3290c7b1737fa9cf47e7fb15d90ea017
Rubén Chavarría <txingo@gmail.com>
2025-10-01 08:38:23 +0200

    ✨ Can select opponent team

--- js/tally/App.jsx
+++ js/tally/App.jsx
@@ -1,21 +1,55 @@
 const App = () => {
+  const defaultOpponentTeam = { name: 'ELIGE UN EQUIPO RIVAL', logo: 'img/new-logos/alcobendas-transparent.png' };
+  const [selectedAge, setSelectedAge] = React.useState('2011');
+  const [selectedOpponentKey, setSelectedOpponentKey] = React.useState('');
   const [tallies, setTallies] = React.useState('99-00');
+  const [opponentTeam, setOpponentTeam] = React.useState(defaultOpponentTeam);
 
   const handleInputChange = (event) => {
     setTallies(event.target.value);
   };
 
+  const handleSelectChange = (event) => {
+    const key = event.target.value;
+    const teams = {
+      coslada: {
+        name: 'COSLADA',
+        logo: 'img/new-logos/coslada-transparent.png',
+      },
+      patrocinio: {
+        name: 'PATROCINIO',
+        logo: 'logo-patrocinio.png',
+      },
+    }
+
+    setOpponentTeam(teams[key]);
+    setSelectedOpponentKey(event.target.value);
+  };
+
+  const handleAgeChange = (event) => {
+    setSelectedAge(event.target.value);
+  };
+
   return (
     <div>
       <div className="data-input">
+        <label htmlFor="age">Año:</label>
+        <select id="age" value={selectedAge} onChange={handleAgeChange}>
+          <option value="2011">2011</option>
+          <option value="2010">2010</option>
+        </select>
+        <br />
+
+        <label htmlFor="opponent">Equipo rival:</label>
+        <select id="opponent" value={selectedOpponentKey} onChange={handleSelectChange}>
+          <option value="">Equipo rival</option>
+          <option value="coslada">Coslada</option>
+          <option value="patrocinio">Patrocinio</option>
+        </select>
+        <br />
+
         <label htmlFor="tallies">Marcador:</label>
-        <input
-          id="tallies"
-          type="text"
-          value={tallies}
-          onChange={handleInputChange}
-          placeholder="Escribe algo..."
-        />
+        <input id="tallies" type="text" value={tallies} onChange={handleInputChange} />
       </div>
 
       <div className="flex-container">
@@ -24,7 +58,7 @@ const App = () => {
             <img src="img/new-logos/fbm-logo-colores-2025-no-ibrcj.png"/>
           </div>
           <div className="col col-headers">
-            <div className="header-title category">Cadete 2011</div>
+            <div className="header-title category">Cadete {selectedAge}</div>
             <FinalResult tallies={tallies} />
           </div>
         </div>
@@ -37,7 +71,7 @@ const App = () => {
 
         <div className="row row-visit-team">
           <div className="col">
-            ZENTRO BASKET
+            {opponentTeam.name}
           </div>
         </div>
 
@@ -47,7 +81,7 @@ const App = () => {
           </div>
           <Tallies tallies={tallies} />
           <div className="col">
-            <img className="logo" src="img/new-logos/zentro-transparent.png"/>
+            <img className="logo" src={opponentTeam.logo} alt="opponent logo" />
           </div>
         </div>
       </div>
